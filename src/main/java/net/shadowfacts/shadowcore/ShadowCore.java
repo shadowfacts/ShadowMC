ebadac366931bd74d3ff71026e64557bb0738e75
Shadowfacts <akorde@icloud.com>
2015-04-04 21:45:52 -0400

    Bump version

--- src/main/java/net/shadowfacts/shadowcore/ShadowCore.java
+++ src/main/java/net/shadowfacts/shadowcore/ShadowCore.java
@@ -21,7 +21,7 @@ public class ShadowCore {
 
 	public static final String modId = "shadowcore";
 	public static final String displayName = "ShadowCore";
-	public static final String version = "1.0.2";
+	public static final String version = "1.0.3";
 	public static final String serverProxy = "net.shadowfacts.shadowcore.proxy.CommonProxy";
 	public static final String clientProxy = "net.shadowfacts.shadowcore.proxy.ClientProxy";
 	
