870a627359ed46703ab49b3efa50194bf4ace31a
Shadowfacts <akorde@icloud.com>
2015-04-04 20:24:59 -0400

    Add more helper methods to Log

--- src/main/java/net/shadowfacts/shadowcore/Log.java
+++ src/main/java/net/shadowfacts/shadowcore/Log.java
@@ -16,16 +16,32 @@ public class Log {
 	public void warn(String msg) {
 		FMLLog.log(this.channel, Level.WARN, msg);
 	}
+
+	public void warn(String msg, Object... args) {
+		warn(String.format(msg, args));
+	}
 	
 	public void error(String msg) {
 		FMLLog.log(this.channel, Level.ERROR, msg);
 	}
-	
+
+	public void error(String msg, Object... args) {
+		error(String.format(msg, args));
+	}
+
 	public void info(String msg) {
 		FMLLog.log(this.channel, Level.INFO, msg);
 	}
-	
+
+	public void info(String msg, Object... args){
+		info(String.format(msg, args));
+	}
+
 	public void debug(String msg) {
 		FMLLog.log(this.channel, Level.DEBUG, msg);
+	}
+
+	public void debug(String msg, Object... args) {
+		debug(String.format(msg, args));
 	}
 }
