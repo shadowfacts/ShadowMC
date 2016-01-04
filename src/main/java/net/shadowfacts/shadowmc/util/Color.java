5fd239bca86acb6702e108d9bf8a6fbee4ec787e
Shadowfacts <me@shadowfacts.net>
2016-01-03 19:27:33 -0500

    Fix color application

--- src/main/java/net/shadowfacts/shadowmc/util/Color.java
+++ src/main/java/net/shadowfacts/shadowmc/util/Color.java
@@ -14,21 +14,21 @@ import net.minecraft.client.renderer.GlStateManager;
 public class Color {
 
 //	MC colors
-	public static final Color BLACK = new Color(0x00000);
-	public static final Color DARK_BLUE = new Color(0x0000AA);
-	public static final Color DARK_GREEN = new Color(0x00AA00);
-	public static final Color DARK_AQUA = new Color(0x00AAAA);
-	public static final Color DARK_RED = new Color(0xAA0000);
-	public static final Color DARK_PURPLE = new Color(0xAA00AA);
-	public static final Color GOLD = new Color(0xFFAA00);
-	public static final Color GRAY = new Color(0xAAAAAA);
-	public static final Color DARK_GRAY = new Color(0x555555);
-	public static final Color BLUE = new Color(0x5555FF);
-	public static final Color GREEN = new Color(0x55FF55);
-	public static final Color AQUA = new Color(0x55FFFF);
-	public static final Color LIGHT_PURPLE = new Color(0xFF55FF);
-	public static final Color YELLOW = new Color(0xFFFF55);
-	public static final Color WHITE = new Color(0xFFFFFF);
+	public static final Color BLACK = new Color(0xFF00000);
+	public static final Color DARK_BLUE = new Color(0xFF0000AA);
+	public static final Color DARK_GREEN = new Color(0xFF00AA00);
+	public static final Color DARK_AQUA = new Color(0xFF00AAAA);
+	public static final Color DARK_RED = new Color(0xFFAA0000);
+	public static final Color DARK_PURPLE = new Color(0xFFAA00AA);
+	public static final Color GOLD = new Color(0xFFFFAA00);
+	public static final Color GRAY = new Color(0xFFAAAAAA);
+	public static final Color DARK_GRAY = new Color(0xFF555555);
+	public static final Color BLUE = new Color(0xFF5555FF);
+	public static final Color GREEN = new Color(0xFF55FF55);
+	public static final Color AQUA = new Color(0xFF55FFFF);
+	public static final Color LIGHT_PURPLE = new Color(0xFFFF55FF);
+	public static final Color YELLOW = new Color(0xFFFFFF55);
+	public static final Color WHITE = new Color(0xFFFFFFFF);
 
 	private float alpha = 1;
 	private float red;
@@ -56,6 +56,6 @@ public class Color {
 	}
 
 	public void apply() {
-		GlStateManager.color(alpha, red, green, blue);
+		GlStateManager.color(red, green, blue, alpha);
 	}
 }
