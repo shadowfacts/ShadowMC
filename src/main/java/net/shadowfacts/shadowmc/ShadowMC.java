e6f652d6744726e7f1d437406cec5007b93ad126
Shadowfacts <me@shadowfacts.net>
2016-01-14 17:09:46 -0500

    Stuff

--- src/main/java/net/shadowfacts/shadowmc/ShadowMC.java
+++ src/main/java/net/shadowfacts/shadowmc/ShadowMC.java
@@ -2,9 +2,12 @@ package net.shadowfacts.shadowmc;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.shadowfacts.shadowmc.command.CommandHandler;
+import net.shadowfacts.shadowmc.compat.CompatRegistrar;
 import net.shadowfacts.shadowmc.config.ConfigManager;
 import net.shadowfacts.shadowmc.event.handler.ShadowMCEventHandler;
 import org.apache.logging.log4j.LogManager;
@@ -30,6 +33,18 @@ public class ShadowMC {
 
 //		Events
 		MinecraftForge.EVENT_BUS.register(new ShadowMCEventHandler());
+
+		CompatRegistrar.preInit(event);
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		CompatRegistrar.init(event);
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		CompatRegistrar.postInit(event);
 	}
 
 	@Mod.EventHandler
