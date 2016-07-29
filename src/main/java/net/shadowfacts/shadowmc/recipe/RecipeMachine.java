957515ed3c04fe98003f8847a07369d9cbec84c2
Shadowfacts <me@shadowfacts.net>
2016-07-29 19:44:25 -0400

    Add a machine recipe framework

---
+++ src/main/java/net/shadowfacts/shadowmc/recipe/RecipeMachine.java
@@ -0,0 +1,14 @@
+package net.shadowfacts.shadowmc.recipe;
+
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+
+/**
+ * @author shadowfacts
+ */
+public interface RecipeMachine {
+
+	List<ItemStack> getInputs();
+
+}
