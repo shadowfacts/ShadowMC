7a474098a0e2ae9458e2d1bb3437cd19a170548e
Shadowfacts <me@shadowfacts.net>
2017-06-14 17:20:05 -0400

    Add RecipeBase

---
+++ src/main/java/net/shadowfacts/shadowmc/recipe/RecipeBase.java
@@ -0,0 +1,11 @@
+package net.shadowfacts.shadowmc.recipe;
+
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.fml.common.registry.IForgeRegistryEntry;
+
+/**
+ * @author shadowfacts
+ */
+public abstract class RecipeBase extends IForgeRegistryEntry.Impl<IRecipe> implements IRecipe {
+
+}
